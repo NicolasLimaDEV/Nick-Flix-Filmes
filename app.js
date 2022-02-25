7188e41e7fe44cbac9d9cb1dfe16db299758f871
Nicolas Lima <91435296+NicolasLimaDEV@users.noreply.github.com>
2022-02-25 14:50:16 -0300

    Add files via upload

---
+++ app.js
@@ -0,0 +1,368 @@
+// Trocar Imagens
+function img01 (){
+    let contA = document.querySelector('.containerA')
+    let contB = document.querySelector('.containerB')
+    let contC = document.querySelector('.containerC')
+    let contD = document.querySelector('.containerD')
+    let contE = document.querySelector('.containerE')
+    let contF = document.querySelector('.containerF')
+    let contG = document.querySelector('.containerG')
+    let contH = document.querySelector('.containerH')
+    let contI = document.querySelector('.containerI')
+    let contJ = document.querySelector('.containerJ')
+    let contK = document.querySelector('.containerK')
+    let contL = document.querySelector('.containerL')
+
+    //Active
+    contA.style.display='flex'
+
+    contB.style.display='none'
+    contC.style.display='none'
+    contD.style.display='none'
+    contE.style.display='none'
+    contF.style.display='none'
+    contG.style.display='none'
+    contH.style.display='none'
+    contI.style.display='none'
+    contJ.style.display='none'
+    contK.style.display='none'
+    contL.style.display='none'
+    
+}
+
+function img02(){
+    let contA = document.querySelector('.containerA')
+    let contB = document.querySelector('.containerB')
+    let contC = document.querySelector('.containerC')
+    let contD = document.querySelector('.containerD')
+    let contE = document.querySelector('.containerE')
+    let contF = document.querySelector('.containerF')
+    let contG = document.querySelector('.containerG')
+    let contH = document.querySelector('.containerH')
+    let contI = document.querySelector('.containerI')
+    let contJ = document.querySelector('.containerJ')
+    let contK = document.querySelector('.containerK')
+    let contL = document.querySelector('.containerL')
+
+    //Active
+    contB.style.display='flex'
+
+    contA.style.display='none'
+    contC.style.display='none'
+    contD.style.display='none'
+    contE.style.display='none'
+    contF.style.display='none'
+    contG.style.display='none'
+    contH.style.display='none'
+    contI.style.display='none'
+    contJ.style.display='none'
+    contK.style.display='none'
+    contL.style.display='none'
+}
+
+function img03(){
+    let contA = document.querySelector('.containerA')
+    let contB = document.querySelector('.containerB')
+    let contC = document.querySelector('.containerC')
+    let contD = document.querySelector('.containerD')
+    let contE = document.querySelector('.containerE')
+    let contF = document.querySelector('.containerF')
+    let contG = document.querySelector('.containerG')
+    let contH = document.querySelector('.containerH')
+    let contI = document.querySelector('.containerI')
+    let contJ = document.querySelector('.containerJ')
+    let contK = document.querySelector('.containerK')
+    let contL = document.querySelector('.containerL')
+
+    //Active
+    contC.style.display='flex'
+
+    contA.style.display='none'
+    contB.style.display='none'
+    contD.style.display='none'
+    contE.style.display='none'
+    contF.style.display='none'
+    contG.style.display='none'
+    contH.style.display='none'
+    contI.style.display='none'
+    contJ.style.display='none'
+    contK.style.display='none'
+    contL.style.display='none'
+}
+
+function img04(){
+    let contA = document.querySelector('.containerA')
+    let contB = document.querySelector('.containerB')
+    let contC = document.querySelector('.containerC')
+    let contD = document.querySelector('.containerD')
+    let contE = document.querySelector('.containerE')
+    let contF = document.querySelector('.containerF')
+    let contG = document.querySelector('.containerG')
+    let contH = document.querySelector('.containerH')
+    let contI = document.querySelector('.containerI')
+    let contJ = document.querySelector('.containerJ')
+    let contK = document.querySelector('.containerK')
+    let contL = document.querySelector('.containerL')
+
+    //Active
+    contD.style.display='flex'
+
+    contA.style.display='none'
+    contB.style.display='none'
+    contC.style.display='none'
+    contE.style.display='none'
+    contF.style.display='none'
+    contG.style.display='none'
+    contH.style.display='none'
+    contI.style.display='none'
+    contJ.style.display='none'
+    contK.style.display='none'
+    contL.style.display='none'
+}
+
+function img05(){
+    let contA = document.querySelector('.containerA')
+    let contB = document.querySelector('.containerB')
+    let contC = document.querySelector('.containerC')
+    let contD = document.querySelector('.containerD')
+    let contE = document.querySelector('.containerE')
+    let contF = document.querySelector('.containerF')
+    let contG = document.querySelector('.containerG')
+    let contH = document.querySelector('.containerH')
+    let contI = document.querySelector('.containerI')
+    let contJ = document.querySelector('.containerJ')
+    let contK = document.querySelector('.containerK')
+    let contL = document.querySelector('.containerL')
+
+    //Active
+    contE.style.display='flex'
+
+    contA.style.display='none'
+    contB.style.display='none'
+    contC.style.display='none'
+    contD.style.display='none'
+    contF.style.display='none'
+    contG.style.display='none'
+    contH.style.display='none'
+    contI.style.display='none'
+    contJ.style.display='none'
+    contK.style.display='none'
+    contL.style.display='none'
+}
+
+function img06(){
+    let contA = document.querySelector('.containerA')
+    let contB = document.querySelector('.containerB')
+    let contC = document.querySelector('.containerC')
+    let contD = document.querySelector('.containerD')
+    let contE = document.querySelector('.containerE')
+    let contF = document.querySelector('.containerF')
+    let contG = document.querySelector('.containerG')
+    let contH = document.querySelector('.containerH')
+    let contI = document.querySelector('.containerI')
+    let contJ = document.querySelector('.containerJ')
+    let contK = document.querySelector('.containerK')
+    let contL = document.querySelector('.containerL')
+
+    //Active
+    contF.style.display='flex'
+
+    contA.style.display='none'
+    contB.style.display='none'
+    contC.style.display='none'
+    contD.style.display='none'
+    contE.style.display='none'
+    contG.style.display='none'
+    contH.style.display='none'
+    contI.style.display='none'
+    contJ.style.display='none'
+    contK.style.display='none'
+    contL.style.display='none'
+}
+
+function img07(){
+    let contA = document.querySelector('.containerA')
+    let contB = document.querySelector('.containerB')
+    let contC = document.querySelector('.containerC')
+    let contD = document.querySelector('.containerD')
+    let contE = document.querySelector('.containerE')
+    let contF = document.querySelector('.containerF')
+    let contG = document.querySelector('.containerG')
+    let contH = document.querySelector('.containerH')
+    let contI = document.querySelector('.containerI')
+    let contJ = document.querySelector('.containerJ')
+    let contK = document.querySelector('.containerK')
+    let contL = document.querySelector('.containerL')
+
+    //Active
+    contG.style.display='flex'
+
+    contA.style.display='none'
+    contB.style.display='none'
+    contC.style.display='none'
+    contD.style.display='none'
+    contE.style.display='none'
+    contF.style.display='none'
+    contH.style.display='none'
+    contI.style.display='none'
+    contJ.style.display='none'
+    contK.style.display='none'
+    contL.style.display='none'
+}
+
+function img08(){
+    let contA = document.querySelector('.containerA')
+    let contB = document.querySelector('.containerB')
+    let contC = document.querySelector('.containerC')
+    let contD = document.querySelector('.containerD')
+    let contE = document.querySelector('.containerE')
+    let contF = document.querySelector('.containerF')
+    let contG = document.querySelector('.containerG')
+    let contH = document.querySelector('.containerH')
+    let contI = document.querySelector('.containerI')
+    let contJ = document.querySelector('.containerJ')
+    let contK = document.querySelector('.containerK')
+    let contL = document.querySelector('.containerL')
+    
+
+    //Active
+    contH.style.display='flex'
+
+    contA.style.display='none'
+    contB.style.display='none'
+    contC.style.display='none'
+    contD.style.display='none'
+    contE.style.display='none'
+    contF.style.display='none'
+    contG.style.display='none'
+    contI.style.display='none'
+    contJ.style.display='none'
+    contK.style.display='none'
+    contL.style.display='none'
+}
+
+function img09(){
+    let contA = document.querySelector('.containerA')
+    let contB = document.querySelector('.containerB')
+    let contC = document.querySelector('.containerC')
+    let contD = document.querySelector('.containerD')
+    let contE = document.querySelector('.containerE')
+    let contF = document.querySelector('.containerF')
+    let contG = document.querySelector('.containerG')
+    let contH = document.querySelector('.containerH')
+    let contI = document.querySelector('.containerI')
+    let contJ = document.querySelector('.containerJ')
+    let contK = document.querySelector('.containerK')
+    let contL = document.querySelector('.containerL')
+    
+
+    //Active
+    contI.style.display='flex'
+
+    contA.style.display='none'
+    contB.style.display='none'
+    contC.style.display='none'
+    contD.style.display='none'
+    contE.style.display='none'
+    contF.style.display='none'
+    contG.style.display='none'
+    contH.style.display='none'
+    contJ.style.display='none'
+    contK.style.display='none'
+    contL.style.display='none'
+}
+
+function img10(){
+    let contA = document.querySelector('.containerA')
+    let contB = document.querySelector('.containerB')
+    let contC = document.querySelector('.containerC')
+    let contD = document.querySelector('.containerD')
+    let contE = document.querySelector('.containerE')
+    let contF = document.querySelector('.containerF')
+    let contG = document.querySelector('.containerG')
+    let contH = document.querySelector('.containerH')
+    let contI = document.querySelector('.containerI')
+    let contJ = document.querySelector('.containerJ')
+    let contK = document.querySelector('.containerK')
+    let contL = document.querySelector('.containerL')
+    
+
+    //Active
+    contJ.style.display='flex'
+
+    contA.style.display='none'
+    contB.style.display='none'
+    contC.style.display='none'
+    contD.style.display='none'
+    contE.style.display='none'
+    contF.style.display='none'
+    contG.style.display='none'
+    contH.style.display='none'
+    contI.style.display='none'
+    contK.style.display='none'
+    contL.style.display='none'
+}
+
+function img11(){
+    let contA = document.querySelector('.containerA')
+    let contB = document.querySelector('.containerB')
+    let contC = document.querySelector('.containerC')
+    let contD = document.querySelector('.containerD')
+    let contE = document.querySelector('.containerE')
+    let contF = document.querySelector('.containerF')
+    let contG = document.querySelector('.containerG')
+    let contH = document.querySelector('.containerH')
+    let contI = document.querySelector('.containerI')
+    let contJ = document.querySelector('.containerJ')
+    let contK = document.querySelector('.containerK')
+    let contL = document.querySelector('.containerL')
+    
+
+    //Active
+    contK.style.display='flex'
+
+    contA.style.display='none'
+    contB.style.display='none'
+    contC.style.display='none'
+    contD.style.display='none'
+    contE.style.display='none'
+    contF.style.display='none'
+    contG.style.display='none'
+    contH.style.display='none'
+    contI.style.display='none'
+    contJ.style.display='none'
+    contL.style.display='none'
+}
+
+function img12(){
+    let contA = document.querySelector('.containerA')
+    let contB = document.querySelector('.containerB')
+    let contC = document.querySelector('.containerC')
+    let contD = document.querySelector('.containerD')
+    let contE = document.querySelector('.containerE')
+    let contF = document.querySelector('.containerF')
+    let contG = document.querySelector('.containerG')
+    let contH = document.querySelector('.containerH')
+    let contI = document.querySelector('.containerI')
+    let contJ = document.querySelector('.containerJ')
+    let contK = document.querySelector('.containerK')
+    let contL = document.querySelector('.containerL')
+    
+
+    //Active
+    contL.style.display='flex'
+
+    contA.style.display='none'
+    contB.style.display='none'
+    contC.style.display='none'
+    contD.style.display='none'
+    contE.style.display='none'
+    contF.style.display='none'
+    contG.style.display='none'
+    contH.style.display='none'
+    contI.style.display='none'
+    contJ.style.display='none'
+    contK.style.display='none'
+}
+
+
